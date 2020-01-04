4b6a5450b25853b782c2a26666855e60793b5555
unknown <dietmar.kling@web.de>
2020-01-04 16:53:28 +0100

    remove old code

--- install/fake-kubelet-service.bat
+++ install/fake-kubelet-service.bat
@@ -46,9 +46,3 @@ echo %COMMAND%
 
 REM space after binpath MUST BE THERE
 sc.exe create %SERVICE_NAME% binpath= "%COMMAND%"
-GOTO :EOF
-:LoCase
-:: Subroutine to convert a variable VALUE to all lower case.
-:: The argument for this subroutine is the variable NAME.
-FOR %%i IN ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") DO CALL SET "%1=%%%1:%%~i%%"
-GOTO:EOF
